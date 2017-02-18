46f3118f20ed45a77728eac7d2beabd0b73277fb
SG0225182 <SG0225182@D9SHZL72.Global.ad.sabre.com>
2017-02-17 22:31:25 -0600

    2/17/2017

--- resources/views/flights/create.blade.php
+++ resources/views/flights/create.blade.php
@@ -1,52 +1,72 @@
-@extends ('layout')
+@extends ('layout') @section ('content')
 
-@section ('content')
+<h1>Add a new flight</h1>
 
-    <h1>Add a new flight</h1>
-
-
-    <div class="row">
-    <form class="col s12">
-        <div class="input-field col s6">
-          <input id="from" type="text" class="validate">
-          <label for="from">From</label>
-        </div>
-        <div class="input-field col s6">
-          <input id="to" type="text" class="validate">
-          <label for="to">To</label>
-        </div>
-      </div>
+<form class="col s12">
 
     <div class="row">
         <div class="input-field col s6">
-          <input id="date" type="text" class="validate datepicker">
-          <label for="date">Date of Departure</label>
+            <input id="date" type="text" class="validate datepicker">
+            <label for="date">Date of Departure</label>
+
         </div>
-        <div class="input-field col s6">
-          <input id="to" type="text" class="validate">
-          <label for="to">To</label>
-        </div>
-      </div>
+    </div>
 
     <div class="row">
         <div class="input-field col s6">
-          <input id="dep_time" type="text" class="validate">
-          <label for="dep_time">Departure Time</label>
+            <input id="from" type="text" class="validate">
+            <label for="from">From</label>
         </div>
         <div class="input-field col s6">
-          <input id="arr_time" type="text" class="validate">
-          <label for="arr_time">Arrival Time</label>
+            <input id="to" type="text" class="validate">
+            <label for="to">To</label>
         </div>
-      </div>
+    </div>
+
+
     <div class="row">
         <div class="input-field col s6">
-          <input id="ac_type" type="text" class="validate">
-          <label for="ac_type">Departure Time</label>
+            <input id="aircraft_type" type="text" class="validate">
+            <label for="to">Aircraft</label>
         </div>
-        
-      </div>
-    </form>
+    </div>
+    </div>
+
+
+    <div class="row">
+        <div class="input-field col s6">
+            <input id="dep_time" type="text" class="validate">
+            <label for="dep_time">Departure Time</label>
+        </div>
+        <div class="input-field col s6">
+            <input id="arr_time" type="text" class="validate">
+            <label for="arr_time">Arrival Time</label>
+        </div>
+    </div>
+
+
+
+
+
+    <div class="row">
+        <div class="input-field col s6">
+            <input id="day_takeoffs" type="text" class="validate">
+            <label for="day_takeoffs">Takeoffs (Day)</label>
+        </div>
+        <div class="input-field col s6">
+            <input id="day_landings" type="text" class="validate">
+            <label for="day_landings">Landings (Night)</label>
+        </div>
+
+    </div>
+</form>
+
+
+
+<div class="input-field col s6">
+    <input id="time_of_dep" type="text" class="validate">
+    <label for="to">Time of Departure</label>
+</div>
+</div>
 
 @stop
-
-    
